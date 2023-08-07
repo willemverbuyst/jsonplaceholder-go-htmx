ab416da254dd739ee461bfe663a14578158f7f00
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-07 18:56:35 +0200

    Init project

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Listen(":3000")
+}
